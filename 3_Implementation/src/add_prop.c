db2b923aba2f3685514f44a84384a240b9644634
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-11-21 12:12:56 +0530

    Create add_prop.c

---
+++ 3_Implementation/src/add_prop.c
@@ -0,0 +1,41 @@
+#include "funs.h"
+#include "string.h"
+//#include"stdio.h"
+
+void add_prop()
+{
+    size = sizeof(u);
+
+    fseek(fptr, 0, SEEK_END);
+    char another = 'y';
+
+    while (another == 'y')
+    {
+        printf("Enter Name :\n ");
+        scanf("%s", &u.name);
+
+        printf("Enter Contact number :\n ");
+        scanf("%ld", &u.cnumber);
+
+        printf("Enter Type(villa | house | land) : \n");
+        scanf("%s", &u.ptype);
+
+        printf("Enter Extent :\n ");
+        scanf("%d", &u.ext);
+
+        printf("Enter place or city :\n ");
+        scanf("%s", &u.place);
+
+        printf("Enter country :\n ");
+        scanf("%s", &u.country);
+
+        fwrite(&u, size, 1, fptr);
+
+        printf("Want to add another property\n");
+        printf("y/n\n");
+        fflush(stdin);
+
+        scanf("%c", &another);
+    }
+} 
+ 
